750e21217d79222d8ea6c245b21a02df46f8c4e6
IgnacioMelgarejo <melgarejonacho@hotmail.com>
2023-02-05 00:30:07 -0300

    Gender and VideoGames Route completle

--- PI-Videogames-main/api/src/routes/index.js
+++ PI-Videogames-main/api/src/routes/index.js
@@ -1,12 +1,15 @@
 const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-
+const videoGamesRouter = require("./videoGamesRouter.js")
+const gender = require("./genderRouter.js")
 
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
+router.use("/videogames", videoGamesRouter);
+router.use("/gender", gender);
 
 
 module.exports = router;
